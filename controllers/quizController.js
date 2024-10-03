b4945ad132a8ea8ad3ee039c750204c7ce7f01d5
abhishek <shriabhishek929@gmail.com>
2024-10-03 10:17:31 +0530

    did some changes in controller and routes

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -10,11 +10,21 @@ exports.getQuizzes = async (req, res) => {
   }
 };
 
-// Create a quiz
+// Create a new quiz
 exports.createQuiz = async (req, res) => {
   const { title, description, questions } = req.body;
+
+  if (questions.length < 5) {
+    return res.status(400).json({ message: 'A quiz must have at least 5 questions.' });
+  }
+
   try {
-    const quiz = new Quiz({ title, description, questions, createdBy: req.user.id });
+    const quiz = new Quiz({
+      title,
+      description,
+      questions,
+      createdBy: req.user.id,
+    });
     await quiz.save();
     res.status(201).json(quiz);
   } catch (error) {
@@ -22,16 +32,57 @@ exports.createQuiz = async (req, res) => {
   }
 };
 
+// Update an existing quiz
+exports.updateQuiz = async (req, res) => {
+  const { title, description, questions } = req.body;
+
+  if (questions.length < 5) {
+    return res.status(400).json({ message: 'A quiz must have at least 5 questions.' });
+  }
+
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+    if (!quiz) {
+      return res.status(404).json({ message: 'Quiz not found' });
+    }
+
+    quiz.title = title || quiz.title;
+    quiz.description = description || quiz.description;
+    quiz.questions = questions || quiz.questions;
+
+    await quiz.save();
+    res.json(quiz);
+  } catch (error) {
+    res.status(400).json({ message: 'Error updating quiz' });
+  }
+};
+
 // Delete a quiz
 exports.deleteQuiz = async (req, res) => {
   try {
     const quiz = await Quiz.findById(req.params.id);
-    if (quiz.createdBy.toString() !== req.user.id) {
-      return res.status(401).json({ message: 'Not authorized to delete this quiz' });
+    if (!quiz) {
+      return res.status(404).json({ message: 'Quiz not found' });
     }
     await quiz.remove();
     res.json({ message: 'Quiz removed' });
   } catch (error) {
     res.status(400).json({ message: 'Error deleting quiz' });
+  }
+};
+
+// Delete a question from a quiz
+exports.deleteQuestionFromQuiz = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.quizId);
+    if (!quiz) {
+      return res.status(404).json({ message: 'Quiz not found' });
+    }
+
+    quiz.questions = quiz.questions.filter(q => q._id.toString() !== req.params.questionId);
+    await quiz.save();
+    res.json({ message: 'Question deleted' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error deleting question' });
   }
 };
--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { getQuizzes, createQuiz, deleteQuiz } = require('../controllers/quizController');
-const { protect } = require('../middleware/authMiddelware');
+const {
+  getQuizzes,
+  createQuiz,
+  updateQuiz,
+  deleteQuiz,
+  deleteQuestionFromQuiz,
+} = require('../controllers/quizController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', getQuizzes);
 router.post('/', protect, createQuiz);
-router.delete('/:id', protect, deleteQuiz);
+router.put('/:id', protect, updateQuiz); // Update quiz
+router.delete('/:id', protect, deleteQuiz); // Delete quiz
+router.delete('/:quizId/question/:questionId', protect, deleteQuestionFromQuiz); // Delete a question from a quiz
 
 module.exports = router;
